37ee6fa039a008052c16e31e56cba1f8756c8b0c
KimYooBin1 <kyb1208tg@gmail.com>
2023-11-19 13:17:12 +0900

    first commit

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
+            <code className={styles.code}>123123pages/index.js</code>
           </p>
         </div>
       </main>
